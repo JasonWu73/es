544baf6cdd341c625c4b17827ff069d1250d80cf
吴仙杰 <wuxianjiezh@gmail.com>
2023-03-11 17:35:06 +0800

    定义路由

--- my-node/src/app.ts
+++ my-node/src/app.ts
@@ -1,6 +1,15 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 // 这里采用 ES 模块写法, TS 会自动转换为 Node.js 可识别的模块语法
 
+import todoRoutes from './routes/todos';
+
 const app = express();
+
+app.use('/todos', todoRoutes);
+
+// error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).send({ message: err.message });
+});
 
 app.listen(3000);
