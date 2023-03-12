3ebf07e24577fcd1e4ebc892155b8e939f37bdb2
吴仙杰 <wuxianjiezh@gmail.com>
2023-03-12 11:22:17 +0800

    REST Todo API

--- my-node/src/controllers/todo.ts
+++ my-node/src/controllers/todo.ts
@@ -1,4 +1,4 @@
-import { RequestHandler } from 'express';
+import { RequestHandler, Request, Response, NextFunction } from 'express';
 import { Todo } from '../models/todo';
 
 const TODOS: Todo[] = [];
@@ -10,3 +10,33 @@ export const createTodo: RequestHandler = (req, res, next) => {
   TODOS.push(newTodo);
   res.status(201).send({ message: 'Created the todo.', createTodo: newTodo })
 };
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.send(TODOS);
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const id = req.params.id;
+  const todo = TODOS.find((todo: Todo) => {
+    return todo.id === id;
+  });
+  if (!todo) {
+    throw new Error('Could not found todo!');
+    // res.status(404).send({ message: 'Could not found todo!' });
+    // return;
+  }
+
+  todo.text = (req.body as { text: string }).text;
+  res.status(200).send({ message: 'Updated!', updateTodo: todo })
+}
+
+export function deleteTodo (req: Request<{ id: string }>, res: Response, next: NextFunction)  {
+  const id = req.params.id;
+  const todoIndex = TODOS.findIndex((todo: Todo) => todo.id === id);
+  if (todoIndex === -1) {
+    throw new Error('Could not found todo!');
+  }
+
+  TODOS.splice(todoIndex, 1);
+  res.status(200).send({ message: 'Deleted!' });
+}
