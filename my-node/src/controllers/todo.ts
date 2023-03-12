c6bae38edbfa6d70013829b872b6c8f662d9b09d
吴仙杰 <wuxianjiezh@gmail.com>
2023-03-12 14:40:44 +0800

    避免 IntelliJ 误报警告（回滚）

--- my-node/src/controllers/todo.ts
+++ my-node/src/controllers/todo.ts
@@ -5,10 +5,8 @@ const TODOS: Todo[] = [];
 
 export const createTodo: RequestHandler = (req, res, next) => {
   const text = (req.body as { text: string }).text;
-  // @ts-ignore
   const newTodo = new Todo(Math.random().toString(), text);
 
-  // @ts-ignore
   TODOS.push(newTodo);
   res.status(201).send({ message: 'Created the todo.', createTodo: newTodo })
 };
@@ -19,12 +17,10 @@ export const getTodos: RequestHandler = (req, res, next) => {
 
 export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
   const id = req.params.id;
-  // @ts-ignore
   const todo = TODOS.find((todo: Todo) => {
     return todo.id === id;
   });
   if (!todo) {
-    // @ts-ignore
     throw new Error('Could not found todo!');
     // res.status(404).send({ message: 'Could not found todo!' });
     // return;
@@ -36,14 +32,11 @@ export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
 
 export function deleteTodo (req: Request<{ id: string }>, res: Response, next: NextFunction)  {
   const id = req.params.id;
-  // @ts-ignore
   const todoIndex = TODOS.findIndex((todo: Todo) => todo.id === id);
   if (todoIndex === -1) {
-    // @ts-ignore
     throw new Error('Could not found todo!');
   }
 
-  // @ts-ignore
   TODOS.splice(todoIndex, 1);
   res.status(200).send({ message: 'Deleted!' });
 }
