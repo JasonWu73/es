9892f8228c12c91b89957e20d0c381f5f0ee5dee
吴仙杰 <wuxianjiezh@gmail.com>
2023-03-15 17:56:51 +0800

    登录表单增加验证规则

--- my-react/src/login/login/Login.tsx
+++ my-react/src/login/login/Login.tsx
@@ -1,5 +1,5 @@
 import styles from './Login.module.scss';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 interface Props {
   onLogin: (username: string, password: string) => void;
@@ -8,19 +8,76 @@ interface Props {
 function Login({ onLogin }: Props) {
   const usernameRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const [ error, setError ] = useState(false);
+
+  const getUsername = () => {
+    return usernameRef.current!.value.trim();
+  };
+
+  const getPassword = () => {
+    return passwordRef.current!.value.trim();
+  };
 
   const loginSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const username = usernameRef.current!.value.trim();
-    const password = passwordRef.current!.value.trim();
+
+    const validUsername = validateUsernameInput();
+    const validPassword = validatePasswordInput();
+    if (!validUsername || !validPassword) return;
+
+    const username = getUsername();
+    const password = getPassword();
     onLogin(username, password);
+  };
+
+  function validateUsernameInput() {
+    const classList = usernameRef.current!.classList;
+    if (getUsername().length <= 2) {
+      classList.add(styles.error);
+      setError(true);
+      return false;
+    }
+
+    classList.remove(styles.error);
+    setError(false);
+    return true;
+  }
+
+  const usernameChangeHandler = () => {
+    validateUsernameInput();
+  };
+
+  function validatePasswordInput() {
+    const classList = passwordRef.current!.classList;
+    if (getPassword().length <= 2) {
+      classList.add(styles.error);
+      setError(true);
+      return false;
+    }
+    classList.remove(styles.error);
+    setError(false);
+    return true;
+  }
+
+  const passwordChangeHandler = () => {
+    validatePasswordInput();
   };
 
   return (
     <form onSubmit={loginSubmitHandler} className={styles.login}>
-      <input ref={usernameRef} type="text" placeholder="Username"/>
-      <input ref={passwordRef} type="password" placeholder="Password"/>
-      <button type="submit">Login</button>
+      <input
+        ref={usernameRef}
+        onChange={usernameChangeHandler}
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        ref={passwordRef}
+        onChange={passwordChangeHandler}
+        type="password"
+        placeholder="Password"
+      />
+      <button type="submit" disabled={error}>Login</button>
     </form>
   );
 }
