71292ce2d9e37a406f19f88db6150a38849819e9
吴仙杰 <wuxianjiezh@gmail.com>
2023-03-19 02:17:11 +0800

    通过 useReducer 实现表单验证

--- my-react/src/login/login/Login.tsx
+++ my-react/src/login/login/Login.tsx
@@ -1,66 +1,123 @@
 import styles from './Login.module.scss';
-import { ChangeEvent, FormEvent, useState } from 'react';
-import useValidationForm from './useValidationForm';
+import { ChangeEvent, FormEvent, useReducer } from 'react';
 
 interface Props {
   onLogin: (username: string, password: string) => void;
 }
 
-export default function Login({ onLogin }: Props) {
-  const [username, setUsername] = useState('');
-  const [invalidUsername, setInvalidUsername] = useState(false);
-  const [password, setPassword] = useState('');
-  const [invalidPassword, setInvalidPassword] = useState(false);
-  const [invalidForm, setInvalidForm] = useState(false);
+interface LoginForm {
+  username: string,
+  invalidUsername: boolean,
+  password: string,
+  invalidPassword: boolean,
+  invalidForm: boolean
+}
 
-  useValidationForm({
-    username,
-    password,
-    setInvalidForm
+interface LoginAction {
+  type: 'changed_username' | 'checked_username' |
+    'changed_password' | 'checked_password',
+  nextUsername?: string, // 下一个 `state` 以 `next` 为前缀
+  nextPassword?: string
+}
+
+function loginFormReducer(state: LoginForm, action: LoginAction): LoginForm {
+  switch (action.type) {
+    case 'changed_username': {
+      return {
+        ...state,
+        username: action.nextUsername!
+      };
+    }
+    case 'checked_username': {
+      const invalidUsername = state.username.length < 3;
+      return {
+        ...state,
+        invalidUsername: invalidUsername,
+        invalidForm: invalidUsername || state.invalidPassword
+      };
+    }
+    case 'changed_password': {
+      return {
+        ...state,
+        password: action.nextPassword!
+      };
+    }
+    case 'checked_password': {
+      const invalidPassword = state.password.length < 3;
+      return {
+        ...state,
+        invalidPassword: invalidPassword,
+        invalidForm: invalidPassword || state.invalidUsername
+      };
+    }
+  }
+
+  throw Error(`Unknown action: ${action.type}`);
+}
+
+export default function Login({ onLogin }: Props) {
+  // `state` 与 `dispatch` 因为属于组件，故可统一使用命名
+  // 而 `reducer` 会放于组件外面，故需要明确命名
+  const [state, dispatch] = useReducer(loginFormReducer, {
+    username: '',
+    invalidUsername: false,
+    password: '',
+    invalidPassword: false,
+    invalidForm: true
   });
 
   function handleLoginSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    onLogin(username, password);
+    onLogin(state.username, state.password);
   }
 
   function handleUsernameChange(event: ChangeEvent<HTMLInputElement>) {
     const username = event.target.value.trim();
-    setUsername(username);
+    dispatch({
+      type: 'changed_username',
+      nextUsername: username
+    });
   }
 
   function handleUsernameBlur() {
-    setInvalidUsername(username.length < 3);
+    dispatch({
+      type: 'checked_username'
+    });
   }
 
   function handlePasswordChange(event: ChangeEvent<HTMLInputElement>) {
     const password = event.target.value.trim();
-    setPassword(password);
+    dispatch({
+      type: 'changed_password',
+      nextPassword: password
+    });
   }
 
   function handlePasswordBlur() {
-    setInvalidPassword(password.length < 3);
+    dispatch({
+      type: 'checked_password'
+    });
   }
 
   return (
     <form onSubmit={handleLoginSubmit} className={styles.login}>
       <input
-        value={username}
+        value={state.username}
         onChange={handleUsernameChange}
         onBlur={handleUsernameBlur}
-        className={invalidUsername ? styles.error : ''}
+        className={state.invalidUsername ? styles.error : ''}
         type="text"
         placeholder="Username"
       />
       <input
-        value={password}
+        value={state.password}
         onChange={handlePasswordChange}
         onBlur={handlePasswordBlur}
-        className={invalidPassword ? styles.error : ''}
+        className={state.invalidPassword ? styles.error : ''}
         type="password"
         placeholder="Password"
       />
-      <button type="submit" disabled={invalidForm}>Login</button>
+      <button type="submit" disabled={state.invalidForm}>Login</button>
     </form>
   );
 }
