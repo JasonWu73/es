0e83e7ed39598a7504e73247416525c2318ad1ac
吴仙杰 <wuxianjiezh@gmail.com>
2023-03-21 10:54:09 +0800

    优化

--- my-react/src/login/login/useValidationForm.ts
+++ my-react/src/login/login/useValidationForm.ts
@@ -3,26 +3,26 @@ import { Dispatch, SetStateAction, useEffect } from 'react';
 interface Props {
   isInvalidUsername: boolean,
   isInvalidPassword: boolean,
-  setInvalidForm: Dispatch<SetStateAction<boolean>>
+  setIsInvalidForm: Dispatch<SetStateAction<boolean>>
 }
 
 export default function useValidationForm(
   {
     isInvalidUsername,
     isInvalidPassword,
-    setInvalidForm
+    setIsInvalidForm
   }: Props
 ): void {
   useEffect(() => {
     console.log('userValidationForm: setup');
     // debounce
     const timeout = setTimeout(() => {
-      setInvalidForm(isInvalidUsername || isInvalidPassword);
+      setIsInvalidForm(isInvalidUsername || isInvalidPassword);
     }, 500);
 
     return () => {
       console.log('userValidationForm: cleanup');
       clearTimeout(timeout);
     };
-  }, [isInvalidUsername, isInvalidPassword, setInvalidForm]);
+  }, [isInvalidUsername, isInvalidPassword, setIsInvalidForm]);
 }
