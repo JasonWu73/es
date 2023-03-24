6f932a16d13516586cef7887311ce5d2eef8e9ca
吴仙杰 <wuxianjiezh@gmail.com>
2023-03-24 13:57:38 +0800

    React.memo

--- vite-react/src/components/demo/DemoOutput.tsx
+++ vite-react/src/components/demo/DemoOutput.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import MyParagraph from './MyParagraph';
 
-export default function DemoOutput({ show }: DemoOutputProps) {
+const DemoOutput = React.memo(function DemoOutput({ show }: DemoOutputProps) {
   console.log('DemoOutput RUNNING');
   return (
     <MyParagraph>{show ? 'This is new!' : ''}</MyParagraph>
   );
-};
+});
+
+export default DemoOutput;
 
 interface DemoOutputProps {
   show: boolean;
--- vite-react/src/components/ui/Button.tsx
+++ vite-react/src/components/ui/Button.tsx
@@ -1,14 +1,16 @@
 import classes from './Button.module.scss';
 import React from 'react';
 
-export default function Button({ onClick, children }: ButtonProps) {
+const Button = React.memo(function Button({ onClick, children }: ButtonProps) {
   console.log('Button RUNNING');
   return (
     <button onClick={onClick} className={classes.btn}>
       {children}
     </button>
   );
-}
+});
+
+export default Button;
 
 interface ButtonProps {
   onClick: () => void;
