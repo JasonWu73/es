2badec2a646372eee2ee90154ac07e00a3322f35
吴仙杰 <wuxianjiezh@gmail.com>
2023-03-29 17:20:12 +0800

    reset project

--- vite-react/src/App.tsx
+++ vite-react/src/App.tsx
@@ -1,31 +1,10 @@
-import './App.scss';
 import React from 'react';
-import DemoList from './components/demo/DemoList';
-import Button from './components/ui/Button';
-
-function getFalse() {
-  console.log('getFalse');
-  return false;
-}
+import './App.scss';
 
 export default function App() {
-  console.log('=====App=====');
-  const [titleState, setTitleState] = React.useState('Original Title');
-  const nums = React.useMemo(() => initNums([11, 10]), []);
-
-  function initNums(initials: number[]) {
-    console.log('init number array');
-    return [...initials, 2, 1, 4, 3];
-  }
-
-  const handleChangeTitleClick = React.useCallback(() => {
-    setTitleState(Math.random().toString());
-  }, []);
-
   return (
     <div className="app">
-      <DemoList title={titleState} items={nums}/>
-      <Button onClick={handleChangeTitleClick}>Change Title</Button>
+      <h2>Class-based Components</h2>
     </div>
   );
 }
