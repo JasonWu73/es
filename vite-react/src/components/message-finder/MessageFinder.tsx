fd27007d0f3e3f6325b457bcb97abdc1d06e8e2c
吴仙杰 <wuxianjiezh@gmail.com>
2023-03-30 11:54:37 +0800

    Same function replace by class based component

--- vite-react/src/components/message-finder/MessageFinder.tsx
+++ vite-react/src/components/message-finder/MessageFinder.tsx
@@ -1,6 +1,51 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import classes from './MessageFinder.module.scss';
 
+export default class MessageFinder extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      filterMessage: ''
+    };
+  }
+
+  componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>) {
+    if (prevState.filterMessage === this.state.filterMessage) {
+      return;
+    }
+    this.props.onFind(this.state.filterMessage);
+  }
+
+  render() {
+    return (
+      <form className={classes.finder}>
+        <label>
+          Message: {' '}
+          <input
+            type="search"
+            value={this.state.filterMessage}
+            onChange={this.handleChange.bind(this)}
+          />
+        </label>
+      </form>
+    );
+  }
+
+  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const enteredMessage = event.target.value.trim();
+    this.setState({ filterMessage: enteredMessage });
+  }
+}
+
+interface Props {
+  onFind: (filterMessage: string) => void;
+}
+
+interface State {
+  filterMessage: string;
+}
+
+/*
 export default function MessageFinder({ onFind }: {
   onFind: (filterMessage: string) => void
 }) {
@@ -27,3 +72,4 @@ function useFilter(filterMessage: string, onFind: (filterMessage: string) => voi
     onFind(filterMessage);
   }, [filterMessage]);
 }
+*/
