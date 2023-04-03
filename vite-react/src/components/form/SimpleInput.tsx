5322301e27503a3040dab531457db89b8332972e
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-03 11:31:59 +0800

    Simplify form validation

--- vite-react/src/components/form/SimpleInput.tsx
+++ vite-react/src/components/form/SimpleInput.tsx
@@ -1,54 +1,40 @@
 import Button from '../ui/Button';
 import classes from './SimpleInput.module.scss';
-import {ChangeEvent, FormEvent, useEffect, useRef, useState} from "react";
+import {ChangeEvent, FormEvent, useState} from "react";
 
 export default function SimpleInput() {
-    const nameRef = useRef<HTMLInputElement>(null);
     const [name, setName] = useState('');
-    const [nameInvalid, setNameInvalid] = useState(true);
     const [nameTouched, setNameTouched] = useState(false);
-
-    useEffect(() => {
-        console.log('nameInvalid: ', nameInvalid);
-    }, [nameInvalid]);
+    const nameInvalid = name.trim().length === 0;
+    const nameInputInvalid = nameInvalid && nameTouched;
 
     function handleNameBlur() {
         setNameTouched(true);
-        setNameInvalid(name.length === 0);
     }
 
     function handleNameChange(event: ChangeEvent<HTMLInputElement>) {
-        const enteredName = event.target.value;
-        setName(enteredName);
-        setNameInvalid(enteredName.trim().length === 0);
+        setName(event.target.value);
     }
 
     function handleSubmit(event: FormEvent) {
         event.preventDefault();
-        setNameTouched(true);
-        const enteredName = name.trim();
-        if (!enteredName) {
-            setNameInvalid(true);
+        if (nameInvalid) {
             return;
         }
-
-        setNameInvalid(false);
-        console.log('useState: ', enteredName);
-        console.log('useRef: ', nameRef.current!.value.trim());
-
+        console.log('useState: ', name.trim());
         setName('');
-        // nameRef.current!.value = '';
+        setNameTouched(false);
     }
 
     const nameClasses = `${classes['form-control']} 
-    ${nameInvalid && nameTouched ? classes['form-control--error'] : ''}`;
+    ${nameInputInvalid ? classes['form-control--error'] : ''}`;
 
     return (
         <form onSubmit={handleSubmit} className={classes.form}>
             <div className={nameClasses}>
                 <label htmlFor="name">Name</label>
-                <input ref={nameRef} value={name} onChange={handleNameChange} onBlur={handleNameBlur} type="text" id="name"/>
-                {nameInvalid && nameTouched && <p className={classes.error}>Name is empty.</p>}
+                <input value={name} onChange={handleNameChange} onBlur={handleNameBlur} type="text" id="name"/>
+                {nameInputInvalid && <p className={classes.error}>Name is empty.</p>}
             </div>
             <div className={classes['form-action']}>
                 <Button>Submit</Button>
