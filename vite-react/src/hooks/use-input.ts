fce58baa3f1a8dc9ee6bc3766c4b9f68569be8fb
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-03 14:29:34 +0800

    Add comment

--- vite-react/src/hooks/use-input.ts
+++ vite-react/src/hooks/use-input.ts
@@ -3,7 +3,7 @@ import {ChangeEvent, useCallback, useState} from "react";
 export function useInput(validate: (value: string) => boolean) {
     const [value, setValue] = useState(''); // 值
     const [touched, setTouched] = useState(false); // 用于判断是否触发 UI 渲染错误
-    const valid = validate(value); // 校验值是否合法
+    const valid = validate(value); // 校验值是否合法, 用于表单的整体验证
     const hasError = touched && !valid; // 用于判断 UI 是否需要渲染错误信息
 
     const handleBlur = useCallback(() => {
