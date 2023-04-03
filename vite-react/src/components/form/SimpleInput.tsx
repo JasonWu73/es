45ddd95d5a40a7008bd628d4bbfbc90b0c4acc6a
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-03 10:33:54 +0800

    Form validate

--- vite-react/src/components/form/SimpleInput.tsx
+++ vite-react/src/components/form/SimpleInput.tsx
@@ -3,8 +3,9 @@ import classes from './SimpleInput.module.scss';
 import {ChangeEvent, FormEvent, useRef, useState} from "react";
 
 export default function SimpleInput() {
-    const [name, setName] = useState('');
     const nameRef = useRef<HTMLInputElement>(null);
+    const [name, setName] = useState('');
+    const [nameInvalid, setNameInvalid] = useState(true);
 
     function handleNameChange(event: ChangeEvent<HTMLInputElement>) {
         setName(event.target.value);
@@ -13,18 +14,28 @@ export default function SimpleInput() {
     function handleSubmit(event: FormEvent) {
         event.preventDefault();
 
-        console.log('useState: ', name);
-        console.log('useRef: ', nameRef.current!.value);
+        const enteredName = name.trim();
+        if (!enteredName) {
+            setNameInvalid(true);
+            return;
+        }
 
-        // setName('');
-        nameRef.current!.value = '';
+        setNameInvalid(false);
+        console.log('useState: ', enteredName);
+        console.log('useRef: ', nameRef.current!.value.trim());
+
+        setName('');
+        // nameRef.current!.value = '';
     }
+
+    const nameClasses = `${classes['form-control']} ${nameInvalid ? classes['form-control--error'] : ''}`;
 
     return (
         <form onSubmit={handleSubmit} className={classes.form}>
-            <div className={classes['form-control']}>
+            <div className={nameClasses}>
                 <label htmlFor="name">Name</label>
                 <input ref={nameRef} value={name} onChange={handleNameChange} type="text" id="name"/>
+                {nameInvalid && <p className={classes.error}>Name is empty.</p>}
             </div>
             <div className={classes['form-action']}>
                 <Button>Submit</Button>
