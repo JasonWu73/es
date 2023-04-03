c7563d698de60121c88c5686109f7cbc31f61464
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-03 09:16:09 +0800

    Compare useState and useRef

--- vite-react/src/components/form/SimpleInput.tsx
+++ vite-react/src/components/form/SimpleInput.tsx
@@ -1,12 +1,30 @@
 import Button from '../ui/Button';
 import classes from './SimpleInput.module.scss';
+import {ChangeEvent, FormEvent, useRef, useState} from "react";
 
 export default function SimpleInput() {
+    const [name, setName] = useState('');
+    const nameRef = useRef<HTMLInputElement>(null);
+
+    function handleNameChange(event: ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value);
+    }
+
+    function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+
+        console.log('useState: ', name);
+        console.log('useRef: ', nameRef.current!.value);
+
+        // setName('');
+        nameRef.current!.value = '';
+    }
+
     return (
-        <form className={classes.form}>
+        <form onSubmit={handleSubmit} className={classes.form}>
             <div className={classes['form-control']}>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name"/>
+                <input ref={nameRef} value={name} onChange={handleNameChange} type="text" id="name"/>
             </div>
             <div className={classes['form-action']}>
                 <Button>Submit</Button>
