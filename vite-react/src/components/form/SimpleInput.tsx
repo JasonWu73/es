2df578387a8075d289db116babcce6a449899609
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-03 10:57:12 +0800

    Form validate fix first invalid status

--- vite-react/src/components/form/SimpleInput.tsx
+++ vite-react/src/components/form/SimpleInput.tsx
@@ -1,19 +1,27 @@
 import Button from '../ui/Button';
 import classes from './SimpleInput.module.scss';
-import {ChangeEvent, FormEvent, useRef, useState} from "react";
+import {ChangeEvent, FormEvent, useEffect, useRef, useState} from "react";
 
 export default function SimpleInput() {
     const nameRef = useRef<HTMLInputElement>(null);
     const [name, setName] = useState('');
     const [nameInvalid, setNameInvalid] = useState(true);
+    const [nameTouched, setNameTouched] = useState(false);
+
+    useEffect(() => {
+        console.log('nameInvalid: ', nameInvalid);
+    }, [nameInvalid]);
 
     function handleNameChange(event: ChangeEvent<HTMLInputElement>) {
-        setName(event.target.value);
+        setNameTouched(true);
+        const enteredName = event.target.value;
+        setName(enteredName);
+        setNameInvalid(enteredName.trim().length === 0);
     }
 
     function handleSubmit(event: FormEvent) {
         event.preventDefault();
-
+        setNameTouched(true);
         const enteredName = name.trim();
         if (!enteredName) {
             setNameInvalid(true);
@@ -28,14 +36,15 @@ export default function SimpleInput() {
         // nameRef.current!.value = '';
     }
 
-    const nameClasses = `${classes['form-control']} ${nameInvalid ? classes['form-control--error'] : ''}`;
+    const nameClasses = `${classes['form-control']} 
+    ${nameInvalid && nameTouched ? classes['form-control--error'] : ''}`;
 
     return (
         <form onSubmit={handleSubmit} className={classes.form}>
             <div className={nameClasses}>
                 <label htmlFor="name">Name</label>
                 <input ref={nameRef} value={name} onChange={handleNameChange} type="text" id="name"/>
-                {nameInvalid && <p className={classes.error}>Name is empty.</p>}
+                {nameInvalid && nameTouched && <p className={classes.error}>Name is empty.</p>}
             </div>
             <div className={classes['form-action']}>
                 <Button>Submit</Button>
