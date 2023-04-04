80176185c68465e8eb094803284de5979214e628
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-04 18:01:20 +0800

    Optimization multiple pages generation

--- vite-react/vite.config.ts
+++ vite-react/vite.config.ts
@@ -1,12 +1,7 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import {resolve, dirname} from 'path';
+import {resolve, dirname, parse} from 'path';
 import pages from './pages';
-
-const pagesInput = {};
-(pages as string[]).forEach(page => {
-  pagesInput[dirname(page).replace(/\//g, '_')] = page;
-});
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,8 +10,19 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        ...pagesInput
+        ...getPagesInput()
       },
     }
   }
 });
+
+function getPagesInput() {
+  const pagesInput = {};
+  (pages as string[]).forEach(page => {
+    const changedDir = dirname(page).replace(/\//g, '_'); // 获取目录名, 并将 `/` 替换为 `_`
+    const filename = parse(page).name; // Get the file name without extension
+    pagesInput[`${changedDir}_${filename}`] = page;
+  });
+  console.log(pagesInput);
+  return pagesInput;
+}
