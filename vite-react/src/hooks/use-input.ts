5d2e93ea1e3cb88d2349a96eb9fd2e30bb019b5a
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-04 10:37:31 +0800

    Change useReducer to useState

--- vite-react/src/hooks/use-input.ts
+++ vite-react/src/hooks/use-input.ts
@@ -1,73 +1,23 @@
-import {ChangeEvent, useCallback, useReducer} from "react";
-
-interface State {
-  value: string;
-  touched: boolean;
-}
-
-interface Action {
-  type: 'set_value' | 'set_touched' | 'reset';
-  nextValue?: string;
-  nextTouched?: boolean;
-}
-
-function reducer(state: State, action: Action) {
-  switch (action.type) {
-    case 'set_value': {
-      return {
-        ...state,
-        value: action.nextValue!
-      };
-    }
-    case 'set_touched': {
-      return {
-        ...state,
-        touched: action.nextTouched!
-      };
-    }
-    case 'reset': {
-      return {
-        value: '',
-        touched: false
-      }
-    }
-  }
-  throw Error('Unknown action: ' + action.type);
-}
+import {ChangeEvent, useCallback, useState} from "react";
 
 export function useInput(validate: (value: string) => boolean) {
-  const [state, dispatch] = useReducer(reducer, {
-    value: '', // 值
-    touched: false // 用于判断是否触发 UI 渲染错误
-  });
-
-  const valid = validate(state.value); // 校验值是否合法, 用于表单的整体验证
-  const hasError = state.touched && !valid; // 用于判断 UI 是否需要渲染错误信息
+  const [value, setValue] = useState(''); // 值
+  const [touched, setTouched] = useState(false); // 用于判断是否触发 UI 渲染错误
+  const valid = validate(value); // 校验值是否合法, 用于表单的整体验证
+  const hasError = touched && !valid; // 用于判断 UI 是否需要渲染错误信息
 
   const handleBlur = useCallback(() => {
-    dispatch({
-      type: 'set_touched',
-      nextTouched: true
-    });
+    setTouched(true);
   }, []);
 
   const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    dispatch({
-      type: 'set_value',
-      nextValue: event.target.value
-    });
+    setValue(event.target.value);
   }, []);
 
   const reset = useCallback(() => {
-    dispatch({type: 'reset'});
+    setValue('');
+    setTouched(false);
   }, []);
 
-  return {
-    value: state.value,
-    valid,
-    hasError,
-    handleBlur,
-    handleChange,
-    reset
-  };
+  return {value, valid, hasError, handleBlur, handleChange, reset};
 }
