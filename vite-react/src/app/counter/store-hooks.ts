ff3743503e9d605250083cd0a2dd53f2392610a6
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-05 20:49:02 +0800

    Add comment to useSelect and useDispatch

--- vite-react/src/app/counter/store-hooks.ts
+++ vite-react/src/app/counter/store-hooks.ts
@@ -2,5 +2,13 @@ import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 import type {RootState, AppDispatch} from './store';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch: () => AppDispatch = useDispatch;
+
+// Reading Data with useSelector
+// The `useSelector` hook lets our component extract whatever pieces of data it needs from the Redux store state:
+// const counter = useAppSelector(state => state.counter);
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+// Dispatching Actions with useDispatch
+// The `useDispatch` hook gives us the actual dispatch method from the Redux store:
+// const dispatch = useAppDispatch();
+export const useAppDispatch: () => AppDispatch = useDispatch;
