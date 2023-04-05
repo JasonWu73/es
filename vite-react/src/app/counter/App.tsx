ac6b709257d615fe2560ef7e39390d501d2020d4
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-05 20:52:11 +0800

    Providing store more normal way

--- vite-react/src/app/counter/App.tsx
+++ vite-react/src/app/counter/App.tsx
@@ -1,6 +1,4 @@
 import './App.scss';
-import {Provider} from 'react-redux';
-import store from './store';
 import React from 'react';
 import Counter from './features/counter/Counter';
 import Card from '../../components/card/Card';
@@ -8,11 +6,9 @@ import Card from '../../components/card/Card';
 export default function App() {
   return (
     <div className="app">
-      <Provider store={store}>
-        <Card>
-          <Counter/>
-        </Card>
-      </Provider>
+      <Card>
+        <Counter/>
+      </Card>
     </div>
   );
 }
--- vite-react/src/app/counter/main.tsx
+++ vite-react/src/app/counter/main.tsx
@@ -1,11 +1,15 @@
 import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+import {Provider} from 'react-redux';
+import store from './store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
-    <App/>
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </StrictMode>
 );
