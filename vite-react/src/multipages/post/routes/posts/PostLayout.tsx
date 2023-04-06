8499ca87b6d54f17fe6ec0e7af39210e186e49f4
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-06 17:04:10 +0800

    Add url params by useSearchParams

--- vite-react/src/multipages/post/routes/posts/PostLayout.tsx
+++ vite-react/src/multipages/post/routes/posts/PostLayout.tsx
@@ -1,9 +1,14 @@
-import {Outlet} from 'react-router-dom';
+import {ChangeEvent, useState} from 'react';
+import {Outlet, useSearchParams} from 'react-router-dom';
 import Card from '../../../../shared/components/card/Card';
 import Nav, {NavRoute} from '../../../../shared/components/nav/Nav';
 import {PostOutletContext} from './post.model';
 
 const ROUTES = [
+  {
+    to: "/posts/new",
+    name: "New Post"
+  },
   {
     to: "/posts/1",
     name: "Post 1"
@@ -11,17 +16,45 @@ const ROUTES = [
   {
     to: "/posts/2",
     name: "Post 2"
-  },
-  {
-    to: "/posts/new",
-    name: "New Post"
   }
 ] as NavRoute[];
 
 export default function PostLayout() {
+  const [searchParams, setSearchParams] = useSearchParams({id: '3'});
+  const postId = +searchParams.get('id')!;
+  const [routes, setRoutes] = useState(() => [...ROUTES, getNewPost(postId)]);
+
+  function handlePostIdChange(event: ChangeEvent<HTMLInputElement>) {
+    const postId = +event.target.value;
+    if (postId < 3) {
+      setSearchParams({id: '3'});
+      return;
+    }
+
+    setSearchParams({id: postId + ''});
+    setRoutes([
+      ...ROUTES,
+      getNewPost(postId)
+    ]);
+  }
+
   return (
     <Card>
-      <Nav routes={ROUTES}/>
+      <Nav routes={routes}/>
+
+      <input
+        type="number"
+        min={3}
+        value={postId}
+        onChange={handlePostIdChange}
+        placeholder="Please enter post ID to search..."
+        style={{
+          padding: '1rem',
+          border: '0.1rem solid #ccc',
+          borderRadius: '0.8rem',
+          width: '50%'
+        }}
+      />
 
       {/* `Outlet` 类似 `props.children` + `context` */}
       <Outlet context={
@@ -31,4 +64,11 @@ export default function PostLayout() {
       }/>
     </Card>
   );
+}
+
+function getNewPost(postId: number) {
+  return {
+    to: `/posts/${postId}`,
+    name: `Post ${postId}`
+  };
 }
