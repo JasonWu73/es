75325e1ce74caf8d508f17006cc9cdb4610fb5fd
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-06 16:16:03 +0800

    Add countdown

--- vite-react/src/shared/components/not-found/NotFound.tsx
+++ vite-react/src/shared/components/not-found/NotFound.tsx
@@ -1,20 +1,34 @@
 import {useNavigate} from 'react-router-dom';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import Card from '../card/Card';
+
+const COUNTDOWN_SECONDS = 5;
 
 export default function NotFound() {
   const navigate = useNavigate();
+  const [countdown, setCountdown] = useState(COUNTDOWN_SECONDS);
 
   useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown(prevCountdown => prevCountdown - 1);
+    }, 1000);
+
     setTimeout(() => {
       navigate("/");
-    }, 5000);
+      // navigate(-1); // Is equivalent to hitting the back button
+    }, COUNTDOWN_SECONDS * 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
+
+  const countdownMessage = `${countdown} second${countdown > 1 ? 's' : ''}`;
 
   return (
     <Card>
       <h2>Sorry! Not Found Page :(</h2>
-      <h3>🛩️: You will return to the homepage in 5 seconds.</h3>
+      <h3>🛩️: You will return to the homepage in {countdownMessage}.</h3>
     </Card>
   );
 }
