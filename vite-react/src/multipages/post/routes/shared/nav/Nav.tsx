d14a34df6c15b843421804e7ede612ff3a0e5cff
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-06 11:16:36 +0800

    Add simple input form route

--- vite-react/src/multipages/post/routes/shared/nav/Nav.tsx
+++ vite-react/src/multipages/post/routes/shared/nav/Nav.tsx
@@ -2,6 +2,7 @@ import classes from './Nav.module.scss';
 import {Link, Route, Routes} from 'react-router-dom';
 import Home from '../../home/Home';
 import PostList from '../../posts/PostList';
+import SimpleInput from '../../form/SimpleInput';
 
 export default function Nav() {
   return (
@@ -10,11 +11,13 @@ export default function Nav() {
         <ul>
           <li className={classes.active}><Link to="/">Home</Link></li>
           <li><Link to="/posts">Posts</Link></li>
+          <li><Link to="/simple-input">Simple Input Form</Link></li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/posts" element={<PostList/>}/>
+        <Route path="/simple-input" element={<SimpleInput/>}/>
       </Routes>
     </>
   );
