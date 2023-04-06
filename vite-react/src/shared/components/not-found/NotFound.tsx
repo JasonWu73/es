b5220e8af7505b9f8237e837d6916994e630229c
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-06 16:05:20 +0800

    Redirect by navigator hook

--- vite-react/src/shared/components/not-found/NotFound.tsx
+++ vite-react/src/shared/components/not-found/NotFound.tsx
@@ -1,5 +1,20 @@
+import {useNavigate} from 'react-router-dom';
+import {useEffect} from 'react';
+import Card from '../card/Card';
+
 export default function NotFound() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 5000);
+  }, []);
+
   return (
-    <h2>Sorry! Not Found Page :(</h2>
+    <Card>
+      <h2>Sorry! Not Found Page :(</h2>
+      <h3>🛩️: You will return to the homepage in 5 seconds.</h3>
+    </Card>
   );
 }
