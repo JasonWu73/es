016e8e5ff35890374f586e710cc01e2729758671
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-07 16:09:22 +0800

    Formatting code

--- vite-react/src/shared/hooks/use-http.ts
+++ vite-react/src/shared/hooks/use-http.ts
@@ -11,21 +11,25 @@ export function useHttp() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
-  const sendRequest = useCallback(async (
-    {method, url, data}: Request,
-    applyData: (data: any) => void
-  ) => {
-    setLoading(true);
-    setError('');
-    try {
-      const response = await axios({method, url, data});
-      applyData(response.data);
-    } catch (err) {
-      setError((err as AxiosError).message);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+  const sendRequest = useCallback(
+    async (
+      {method, url, data}: Request,
+      applyData: (data: any) => void
+    ) => {
+      setLoading(true);
+      setError('');
+
+      try {
+        const response = await axios({method, url, data});
+        applyData(response.data);
+      } catch (err) {
+        setError((err as AxiosError).message);
+      } finally {
+        setLoading(false);
+      }
+    },
+    []
+  );
 
   return {loading, error, sendRequest};
 }
--- vite-react/src/shared/hooks/use-input.ts
+++ vite-react/src/shared/hooks/use-input.ts
@@ -3,21 +3,31 @@ import {ChangeEvent, useCallback, useState} from "react";
 export function useInput(validate: (value: string) => boolean) {
   const [value, setValue] = useState(''); // 值
   const [touched, setTouched] = useState(false); // 用于判断是否触发 UI 渲染错误
+
   const valid = validate(value); // 校验值是否合法, 用于表单的整体验证
   const hasError = touched && !valid; // 用于判断 UI 是否需要渲染错误信息
 
-  const handleBlur = useCallback(() => {
-    setTouched(true);
-  }, []);
+  const handleBlur = useCallback(
+    () => {
+      setTouched(true);
+    },
+    []
+  );
 
-  const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-  }, []);
+  const handleChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      setValue(event.target.value);
+    },
+    []
+  );
 
-  const reset = useCallback(() => {
-    setValue('');
-    setTouched(false);
-  }, []);
+  const reset = useCallback(
+    () => {
+      setValue('');
+      setTouched(false);
+    },
+    []
+  );
 
   return {value, valid, hasError, handleBlur, handleChange, reset};
 }
