42bb41c383cee80fc4dc872983f03cb83f93cf14
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-07 14:37:18 +0800

    Antd chinese language config

--- vite-react/src/main.tsx
+++ vite-react/src/main.tsx
@@ -1,15 +1,27 @@
-import './index.scss';
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+
+// 由于 antd 组件的默认文案是英文, 所以需要修改为中文
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+import zhCN from 'antd/locale/zh_CN';
+import 'antd/dist/reset.css';
+import {ConfigProvider} from 'antd';
+
+import './index.scss';
+
+dayjs.locale('zh-cn');
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App/>
+      <ConfigProvider locale={zhCN}>
+        <App/>
+      </ConfigProvider>
     </BrowserRouter>
   </StrictMode>
 );
