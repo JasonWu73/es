399dc73db12d994daf70ff7dab9c5841c4121516
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-07 13:55:01 +0800

    Simple redux demo action

--- my-node/src/redux/redux-demo.ts
+++ my-node/src/redux/redux-demo.ts
@@ -3,17 +3,35 @@ import {createStore} from 'redux';
 const store = createStore(counterReducer);
 console.log('initialData: ', store.getState());
 
-store.subscribe(handleCount);
+const unsubscribe = store.subscribe(handleCount);
 
 store.dispatch({type: 'increment'});
+store.dispatch({type: 'decrement'});
+
+unsubscribe();
 
 function handleCount() {
   const latestState = store.getState();
   console.log('latestState: ', latestState);
 }
 
-function counterReducer(state: { counter: number } = {counter: 0}) {
-  return {
-    counter: state.counter + 1
-  };
+function counterReducer(
+  state: { counter: number } = {counter: 0},
+  action: { type: 'increment' | 'decrement' }
+) {
+  switch (action.type) {
+    case 'increment': {
+      return {
+        counter: state.counter + 1
+
+      };
+    }
+    case 'decrement': {
+      return {
+        counter: state.counter - 1
+      };
+    }
+  }
+
+  return state;
 }
