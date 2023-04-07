8063d1569d3ec6a4c6d7af20a37056c16e89da1c
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-07 11:18:28 +0800

    Add empty line and switch import style statement

--- vite-react/src/multipages/counter/main.tsx
+++ vite-react/src/multipages/counter/main.tsx
@@ -1,11 +1,12 @@
+import './index.scss';
 import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import {Provider} from 'react-redux';
 import store from './store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <StrictMode>
     {/* React-Redux allows React components to interact with a Redux store */}
--- vite-react/src/multipages/post/main.tsx
+++ vite-react/src/multipages/post/main.tsx
@@ -1,10 +1,11 @@
+import './index.scss';
 import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from './App';
 import {HashRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <StrictMode>
     {/* Use `BrowserRouter` if not in multiple page */}
