62533be2c825806a49aceee7aa9d7e6169468730
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-07 15:48:20 +0800

    Add comment

--- vite-react/src/multipages/counter/main.tsx
+++ vite-react/src/multipages/counter/main.tsx
@@ -9,6 +9,9 @@ const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <StrictMode>
+    {/* React-Redux allows React components to interact with a Redux store */}
+    {/* Wrapping the app with `<Provider store={store}>` enables all components to use the store */}
+    {/* Global state should go in the Redux store, local state should stay in React components */}
     <Provider store={store}>
       <App/>
     </Provider>
