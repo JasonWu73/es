6a7b8b06eac058c0be2b10131c5fce7326d0b82f
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-12 14:27:00 +0800

    从 Redux 中读取文章详情

--- vite-react/src/routes/post/Post.tsx
+++ vite-react/src/routes/post/Post.tsx
@@ -1,13 +1,16 @@
-import Card from '../../shared/components/card/Card';
 import {usePageTitle} from '../../shared/hooks/use-page-title';
 import {useParams} from 'react-router-dom';
 import {Dispatch, SetStateAction, useEffect, useState} from 'react';
-import {Button} from 'antd';
+import {Button, Space, Typography} from 'antd';
+import {useAppSelector} from '../../store-hooks';
+import {Post as PostType} from './post-slice';
 
 export default function Post() {
   const {id} = useParams();
 
   usePageTitle(`文章详情 - ${id}`);
+
+  const post = usePost(+id!);
 
   const [countdown, setCountdown] = useCountdownTimer(5);
 
@@ -16,12 +19,41 @@ export default function Post() {
   }
 
   return (
-    <Card>
-      <h2>文章详情 - {id}</h2>
-      <h3>{countdown}</h3>
-      <Button onClick={handleResetClick}>Reset</Button>
-    </Card>
+    <Space direction="vertical" size="large">
+      <Typography.Title level={2}>{id} - {post?.title ?? ''}</Typography.Title>
+
+      <section style={{display: 'flex', alignItems: 'center', gap: '1rem'}}>
+        <Typography.Title level={3} style={{marginBottom: 0}}>{countdown}</Typography.Title>
+        <Button onClick={handleResetClick}>Reset</Button>
+      </section>
+
+      <section>
+        <Typography.Text>{post?.body ?? ''}</Typography.Text>
+      </section>
+
+      <section>
+        <Typography.Text>用户 ID：{post?.userId ?? ''}</Typography.Text>
+      </section>
+    </Space>
   );
+}
+
+function usePost(postId: number) {
+  const {posts: cachedPosts} = useAppSelector(state => state.post);
+  const [post, setPost] = useState<PostType | null>(null);
+
+  useEffect(
+    () => {
+      const cachedPost = cachedPosts.find(post => post.id === postId);
+
+      if (!cachedPost) return;
+
+      setPost(cachedPost);
+    },
+    [postId]
+  );
+
+  return post;
 }
 
 function useCountdownTimer(countdownSeconds: number): [number, Dispatch<SetStateAction<number>>] {
