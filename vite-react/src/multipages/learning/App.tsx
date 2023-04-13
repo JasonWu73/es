4314a52a7e880b0eb887c8f8f354abec4de65201
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-13 18:24:14 +0800

    Defining routes

--- vite-react/src/multipages/learning/App.tsx
+++ vite-react/src/multipages/learning/App.tsx
@@ -1,11 +1,17 @@
 import './App.scss';
 import Card from '../../components/card/Card';
+import {createHashRouter, RouterProvider} from 'react-router-dom';
+import Home from './routes/home/Home';
+
+const router = createHashRouter([
+  {path: '/', element: <Home/>}
+]);
 
 export default function App() {
   return (
     <div className="app">
       <Card>
-        <h1>Learning</h1>
+        <RouterProvider router={router}/>
       </Card>
     </div>
   );
