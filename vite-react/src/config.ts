7025f42ae16cb93e230f44911609ec05b5f82f78
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-14 16:27:53 +0800

    添加配置及定义全局类型

--- vite-react/src/config.ts
+++ vite-react/src/config.ts
@@ -1,12 +1,3 @@
-interface ConfigItem {
-  baseUrl: string;
-}
-
-type Config = ConfigItem | undefined;
-
 export function getInternalApiBaseUrl() {
-  // @ts-ignore
-  const config = window._CONFIG as Config;
-
-  return config?.baseUrl || window.location.origin;
+  return window._CONFIG?.apiBaseUrl || window.location.origin;
 }
---
+++ vite-react/src/global.d.ts
@@ -0,0 +1,5 @@
+interface Window {
+  _CONFIG: {
+    apiBaseUrl: string;
+  };
+}
