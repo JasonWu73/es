30fc1944027b832758fe9e1a75ad288103942428
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-14 10:15:46 +0800

    Create routes by component

--- vite-react/src/multipages/learning/App.tsx
+++ vite-react/src/multipages/learning/App.tsx
@@ -1,13 +1,28 @@
 import './App.scss';
 import Card from '../../components/card/Card';
-import {createHashRouter, RouterProvider} from 'react-router-dom';
+import {createHashRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
 import Home from './routes/Home';
 import ProductList from './routes/ProductList';
+import NewProduct from './routes/NewProduct';
+import ProductDetail from './routes/ProductDetail';
+import NotFound from '../../components/not-found/NotFound';
 
-const router = createHashRouter([
-  {path: '/', element: <Home/>},
-  {path: '/products', element: <ProductList/>}
-]);
+// const router = createHashRouter([
+//   {path: '/', element: <Home/>},
+//   {path: '/products', element: <ProductList/>}
+// ]);
+
+const router = createHashRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="*" element={<NotFound/>}/>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/products" element={<ProductList/>}/>
+      <Route path="/products/:id" element={<ProductDetail/>}/>
+      <Route path="/products/new" element={<NewProduct/>}/>
+    </Route>
+  )
+);
 
 export default function App() {
   return (
