a6eb089ddc921f335fdc51e983ad9434570fd6a8
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-14 13:27:00 +0800

    Navigating programmatically

--- vite-react/src/multipages/learning/routes/Home.tsx
+++ vite-react/src/multipages/learning/routes/Home.tsx
@@ -1,10 +1,22 @@
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import {usePageTitle} from '../../../hooks/use-page-title';
 
 export default function Home() {
+  usePageTitle('Home');
+
+  const navigate = useNavigate();
+
+  function handleNavigateClick() {
+    navigate('/products');
+  }
+
   return (
     <>
       <h1>My Home Page</h1>
       <p>Go to <Link to="/products">the list of products.</Link></p>
+      <p>
+        <button onClick={handleNavigateClick}>Navigate</button>
+      </p>
     </>
   );
 }
--- vite-react/src/multipages/learning/routes/ProductList.tsx
+++ vite-react/src/multipages/learning/routes/ProductList.tsx
@@ -1,3 +1,7 @@
+import {usePageTitle} from '../../../hooks/use-page-title';
+
 export default function ProductList() {
+  usePageTitle('Product List');
+
   return <h1>Product List</h1>
 };
