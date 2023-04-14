769be48e5d8bb95f36e3c0cc6c6d711b34d049a8
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-14 09:54:25 +0800

    Add products route

--- vite-react/src/multipages/learning/App.tsx
+++ vite-react/src/multipages/learning/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import Card from '../../components/card/Card';
 import {createHashRouter, RouterProvider} from 'react-router-dom';
-import Home from './routes/home/Home';
+import Home from './routes/Home';
+import ProductList from './routes/ProductList';
 
 const router = createHashRouter([
-  {path: '/', element: <Home/>}
+  {path: '/', element: <Home/>},
+  {path: '/products', element: <ProductList/>}
 ]);
 
 export default function App() {
