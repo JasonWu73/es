d31057de7ac5e828825dd429c5d7fe7fb19015ff
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-17 09:44:27 +0800

    精简按钮组件样式

--- vite-react/src/components/button/Button.tsx
+++ vite-react/src/components/button/Button.tsx
@@ -1,12 +1,19 @@
 import classes from './Button.module.scss';
-import {ReactNode} from "react";
+import {ReactNode} from 'react';
 
-export default function Button({onClick, type = 'submit', children, disabled}: {
-  children: ReactNode;
-  type?: 'submit' | 'button';
-  onClick?: () => void;
-  disabled?: boolean;
-}) {
+export default function Button(
+  {
+    onClick,
+    type = 'submit',
+    children,
+    disabled
+  }: {
+    children: ReactNode;
+    type?: 'submit' | 'button';
+    onClick?: () => void;
+    disabled?: boolean;
+  }
+) {
   return (
     <button
       type={type}
--- vite-react/src/multipages/learning/routes/Root.tsx
+++ vite-react/src/multipages/learning/routes/Root.tsx
@@ -1,10 +1,12 @@
 import {Outlet} from 'react-router-dom';
 import MainNavigation from '../components/MainNavigation';
+import Button from '../../../components/button/Button';
 
 export default function Root() {
   return (
     <>
       <MainNavigation/>
+      <Button disabled>Button</Button>
       <main>
         <Outlet/>
       </main>
