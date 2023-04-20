2d55f6d28daab1393af9502a862cd60b0f87a611
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-20 16:50:03 +0800

    Pre-rendering and Fetch Data

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -2,8 +2,19 @@ import Layout, {siteTitle} from '../components/layout';
 import Head from 'next/head';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import {getSortedPostsData} from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData
+    }
+  };
+}
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -12,8 +23,22 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>我是我，你又是谁？</p>
         <p>
-          Go to <Link href='/posts/first-post'>first post page</Link>
+          Go to <Link href="/posts/first-post">first post page</Link>
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({id,  date, title}) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br/>
+              {id}
+              <br/>
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
