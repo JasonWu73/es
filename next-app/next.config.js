32be089bb7f2611db37c380721010c059f5bb250
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-20 23:06:59 +0800

    配置导出静态站点

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // 导出静态网站
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
