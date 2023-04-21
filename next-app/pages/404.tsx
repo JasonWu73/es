e2a9145281ead6ee1f03325cce33c02f511ec1a2
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-21 16:18:07 +0800

    Polishing custom 404

--- next-app/pages/404.tsx
+++ next-app/pages/404.tsx
@@ -1,3 +1,14 @@
+import Layout from '@/components/layout/layout';
+import Head from 'next/head';
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found :(</h1>;
+  return (
+    <Layout>
+      <Head>
+        <title>页面不存在</title>
+      </Head>
+
+      <h2 className="text-2xl font-bold">您访问的页面不存在 :(</h2>
+    </Layout>
+  );
 };
