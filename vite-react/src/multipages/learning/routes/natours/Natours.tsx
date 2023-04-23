2544923c29baf8230d79ec55598c10249a00c378
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-23 14:19:54 +0800

    Building header

--- vite-react/src/multipages/learning/routes/natours/Natours.tsx
+++ vite-react/src/multipages/learning/routes/natours/Natours.tsx
@@ -1,5 +1,15 @@
+import React from 'react';
+import classes from './Natours.module.scss';
+import { usePageTitle } from '../../../../hooks/use-page-title';
+
 export default function Natours() {
+  usePageTitle('Natours');
+
   return (
-    <h1>Natours</h1>
+    <div className={classes.natours}>
+      <header className={classes.header}>
+        Some text...
+      </header>
+    </div>
   );
 }
