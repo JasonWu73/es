005b94bac12f911c483439c8de60690c937a59c5
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-24 18:07:37 +0800

    change home page title

--- vite-react/src/multipages/learning/routes/home/Home.tsx
+++ vite-react/src/multipages/learning/routes/home/Home.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './Home.module.scss';
+import { usePageTitle } from '@/hooks/use-page-title';
 import { Link } from 'react-router-dom';
 
 export default function Home() {
+  usePageTitle('Learning');
+
   return (
     <>
       <nav>
-        <ul className={styles['nav']}>
+        <ul className={styles.nav}>
           <li><Link to="/natours">Natours</Link></li>
         </ul>
       </nav>
