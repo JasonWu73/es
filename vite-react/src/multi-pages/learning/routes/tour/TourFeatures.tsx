4baf062b80ef0309655df49107e859c4c9e1c47d
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-26 10:27:02 +0800

    重构代码

--- vite-react/src/multi-pages/learning/routes/tour/TourFeatures.tsx
+++ vite-react/src/multi-pages/learning/routes/tour/TourFeatures.tsx
@@ -1,58 +1,46 @@
 import React from 'react';
 import '@/assets/linea-icons/styles.css';
 
+const FEATURES = [
+  {
+    icon: 'icon-basic-world',
+    title: '探索世界',
+    body: '在大自然的怀抱中，我们可以栉风沐雨，临山观水；可以聆听鸟鸣欢歌，松涛海浪；可以感受来自田园的清新馨香的自然之风；在大自然的怀抱中我们不需要隐匿什么，也不需要雕饰什么，一切都是那样的自如随意；在大自然的怀抱中，我们可以暂时摆脱一切烦恼。'
+  },
+  {
+    icon: 'icon-basic-compass',
+    title: '遇见大自然',
+    body: '面对着奔涌的江流，江水拍击着堤岸，感受着大自然中时光的流逝，犹如这东逝的水一般，昙花的一现，告诉我们，越是美丽的事物，它的美才是最值得珍惜的，错过了，无需感伤，杨柳枯了，有再青的时候；花儿谢了，也有再开的时候，这便是自然的轮回。'
+  },
+  {
+    icon: 'icon-basic-map',
+    title: '找到你的方式',
+    body: '你可以登上高高的山冈，找一片空地躺下，对着蓝天，吸吮着它的深邃，它的一尘不染的芳馥。闭上双眼，嘴里潇洒地衔着一片被季节遗弃的枫叶，任微笑的阳光拂过，拂过那没有一丝皴皱的心田。清风吹拂，摇曳大自然的风铃；黄莺歌咏，鸣啭着大自然的心音。'
+  },
+  {
+    icon: 'icon-basic-heart',
+    title: '更健康的生活',
+    body: '远离屏幕，将为您的心理健康和家人的安宁创造奇迹。在户外共度美好时光也是加深家庭纽带和留下独特回忆的好方法。在大自然中度过的时间是值得的。它不仅可以减轻压力，而且自然环境还可以改善您的情绪并改善身心健康。外面时间的价值怎么强调都不为过。'
+  }
+];
+
 export default function TourFeatures() {
   return (
     <section className="tour-section-features">
       <div className="row">
-        <div className="col-1-of-4">
-          <div className="feature-box">
-            <i className="feature-box__icon icon-basic-world" />
-            <h3 className="tour-heading-tertiary u-margin-bottom-small">探索世界</h3>
-            <p className="feature-box__text">
-              在大自然的怀抱中，我们可以栉风沐雨，临山观水；可以聆听鸟鸣欢歌，松涛海浪；
-              可以感受来自田园的清新馨香的自然之风；在大自然的怀抱中我们不需要隐匿什么，
-              也不需要雕饰什么，一切都是那样的自如随意；在大自然的怀抱中，我们可以暂时摆脱一切烦恼，
-              让思想进入一种脱俗而不羁的境界。
-            </p>
+        {FEATURES.map(feature => (
+          <div key={feature.icon} className="col-1-of-4">
+            <div className="feature-box">
+              <i className={`feature-box__icon ${feature.icon}`} />
+              <h3 className="tour-heading-tertiary u-margin-bottom-small">
+                {feature.title}
+              </h3>
+              <p className="feature-box__text">
+                {feature.body}
+              </p>
+            </div>
           </div>
-        </div>
-
-        <div className="col-1-of-4">
-          <div className="feature-box">
-            <i className="feature-box__icon icon-basic-compass" />
-            <h3 className="tour-heading-tertiary u-margin-bottom-small">遇见大自然</h3>
-            <p className="feature-box__text">
-              子在川上曰：“逝者如斯乎”面对着奔涌的江流，江水拍击着堤岸，感受着大自然中时光的流逝，
-              犹如这东逝的水一般，昙花的一现，告诉我们，越是美丽的事物，它的美才是最值得珍惜的，
-              错过了，无需感伤，杨柳枯了，有再青的时候；花儿谢了，也有再开的时候，这便是自然的轮回。
-            </p>
-          </div>
-        </div>
-
-        <div className="col-1-of-4">
-          <div className="feature-box">
-            <i className="feature-box__icon icon-basic-map" />
-            <h3 className="tour-heading-tertiary u-margin-bottom-small">找到你的方式</h3>
-            <p className="feature-box__text">
-              投身大自然，你可以登上高高的山冈，找一片空地躺下，对着蓝天，吸吮着它的深邃，它的一尘不染的芳馥。
-              闭上双眼，嘴里潇洒地衔着一片被季节遗弃的枫叶，任微笑的阳光拂过，拂过那没有一丝皴皱的心田。
-              清风吹拂，摇曳大自然的风铃；黄莺歌咏，鸣啭着大自然的心音。
-            </p>
-          </div>
-        </div>
-
-        <div className="col-1-of-4">
-          <div className="feature-box">
-            <i className="feature-box__icon icon-basic-heart" />
-            <h3 className="tour-heading-tertiary u-margin-bottom-small">过上更健康的生活</h3>
-            <p className="feature-box__text">
-              远离屏幕，将为您的心理健康和家人的安宁创造奇迹。在户外共度美好时光也是加深家庭纽带和留下独特回忆的好方法。
-              在大自然中度过的时间是值得的。它不仅可以减轻压力，而且自然环境还可以改善您的情绪并改善身心健康。
-              外面时间的价值怎么强调都不为过。
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
--- vite-react/src/multi-pages/learning/routes/tour/TourMain.tsx
+++ vite-react/src/multi-pages/learning/routes/tour/TourMain.tsx
@@ -5,6 +5,35 @@ import photo3 from '@/assets/tour/img/nat-3-large.jpg';
 import TourFeatures from '@/multi-pages/learning/routes/tour/TourFeatures';
 import Tours from '@/multi-pages/learning/routes/tour/Tours';
 
+const ABOUTS = [
+  {
+    title: '大自然的五彩缤纷',
+    body: '大自然是一个神奇的魔术师，把世界万物变得五颜六色。每个季节都十分美丽，但春姑娘的颜色是最多的。这都是大自然给的。大自然给予了春天许许多多的东西。是大自然把枯萎的树皮洒满了红、白、绿等不同的颜色；是大自然把山坡点缀得五彩缤纷；是大自然把森林协奏曲谱上了乐章；是大自然为花儿穿上美丽、鲜艳的衣裳，喷上馥郁的清香，使它们个个争妍斗艳，好不美丽。'
+  },
+  {
+    title: '大自然好像一首曲',
+    body: '大自然好像一首曲，一首无边无际的曲，每个音符都带有动听的音律，每个音节都带着欢快的节奏，每个音段都带有柔美和安适，歌曲自然而不失感点，多似水中有动的鱼儿，自由，愉快。这首曲载着倾听者无虑的梦想，使倾听者感受曲中大自然的鸟语花香，大自然的多彩芬芳，思绪沉沦在大自然如此令人向往之中。'
+  }
+];
+
+const PHOTOS = [
+  {
+    src: photo1,
+    alt: 'Photo 1',
+    id: 'p1'
+  },
+  {
+    src: photo2,
+    alt: 'Photo 2',
+    id: 'p2'
+  },
+  {
+    src: photo3,
+    alt: 'Photo 3',
+    id: 'p3'
+  }
+];
+
 export default function TourMain() {
   return (
     <main>
@@ -17,19 +46,16 @@ export default function TourMain() {
 
         <div className="row">
           <div className="col-1-of-2">
-            <h3 className="tour-heading-tertiary u-margin-bottom-small">大自然的五彩缤纷</h3>
-            <p className="tour-paragraph">
-              大自然是一个神奇的魔术师，把世界万物变得五颜六色。每个季节都十分美丽，但春姑娘的颜色是最多的。这都是大自然给的。
-              大自然给予了春天许许多多的东西。是大自然把枯萎的树皮洒满了红、白、绿等不同的颜色；是大自然把山坡点缀得五彩缤纷；
-              是大自然把森林协奏曲谱上了乐章；是大自然为花儿穿上美丽、鲜艳的衣裳，喷上馥郁的清香，使它们个个争妍斗艳，好不美丽。
-            </p>
-
-            <h3 className="tour-heading-tertiary u-margin-bottom-small">大自然好像一首曲</h3>
-            <p className="tour-paragraph">
-              大自然好像一首曲，一首无边无际的曲，每个音符都带有动听的音律，每个音节都带着欢快的节奏，每个音段都带有柔美和安适，
-              歌曲自然而不失感点，多似水中有动的鱼儿，自由，愉快。这首曲载着倾听者无虑的梦想，使倾听者感受曲中大自然的鸟语花香，
-              大自然的多彩芬芳，思绪沉沦在大自然如此令人向往之中。
-            </p>
+            {ABOUTS.map(about => (
+              <React.Fragment key={about.title}>
+                <h3 className="tour-heading-tertiary u-margin-bottom-small">
+                  {about.title}
+                </h3>
+                <p className="tour-paragraph">
+                  {about.body}
+                </p>
+              </React.Fragment>
+            ))}
 
             <a
               href="#"
@@ -41,21 +67,14 @@ export default function TourMain() {
           </div>
           <div className="col-1-of-2">
             <div className="composition">
-              <img
-                src={photo1}
-                alt="Photo 1"
-                className="composition__photo composition__photo--p1"
-              />
-              <img
-                src={photo2}
-                alt="Photo 2"
-                className="composition__photo composition__photo--p2"
-              />
-              <img
-                src={photo3}
-                alt="Photo 3"
-                className="composition__photo composition__photo--p3"
-              />
+              {PHOTOS.map(photo => (
+                <img
+                  key={photo.id}
+                  src={photo.src}
+                  alt={photo.alt}
+                  className={`composition__photo composition__photo--${photo.id}`}
+                />
+              ))}
             </div>
           </div>
         </div>
