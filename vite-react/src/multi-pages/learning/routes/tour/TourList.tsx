ee93bbcd561d36180ae7c68873db5c0fbf4a3d29
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-27 09:54:02 +0800

    旅行卡片

--- vite-react/src/multi-pages/learning/routes/tour/TourList.tsx
+++ vite-react/src/multi-pages/learning/routes/tour/TourList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Tours() {
+export default function TourList() {
   return (
     <section className="tour-section-tours">
       <div className="u-center-text u-margin-bottom-big">
@@ -13,7 +13,23 @@ export default function Tours() {
         <div className="col-1-of-3">
           <div className="card">
             <div className="card__side card__side--front">
-              Front Side
+              <div className="card__picture card__picture--1">
+                &nbsp;
+              </div>
+              <h4 className="card__heading">
+                <span className="card__heading-span card__heading-span--1">
+                  海洋探险家
+                </span>
+              </h4>
+              <div className="card__details">
+                <ul>
+                  <li>3日游</li>
+                  <li>最多 30 人</li>
+                  <li>2 名导游</li>
+                  <li>舒适的酒店</li>
+                  <li>难度：容易</li>
+                </ul>
+              </div>
             </div>
 
             <div className="card__side card__side--back card__side--back-1">
