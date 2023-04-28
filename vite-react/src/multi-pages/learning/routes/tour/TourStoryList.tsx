5ba8bf1e73cc32c0a8ed05cb2ffd4ba9e5efbc1f
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-28 17:36:36 +0800

    图片动态缩放及模糊效果

--- vite-react/src/multi-pages/learning/routes/tour/TourStoryList.tsx
+++ vite-react/src/multi-pages/learning/routes/tour/TourStoryList.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import nat8 from '@/assets/tour/img/nat-8.jpg';
+import nat9 from '@/assets/tour/img/nat-9.jpg';
 
 const STORIES = [
   {
     id: 1,
+    person: '叶蔷薇',
     title: '我和家人度过了最美好的一周',
     body: '工作别太疲惫，心情别太郁闷，没事多喝点水，周末多多聚会，友情最为珍贵，朋友真心相对，生活添点趣味，天天快乐相随!',
     imgSrc: nat8
+  },
+  {
+    id: 2,
+    person: '吴仙杰',
+    title: '哇！我的生活现次变得不同了',
+    body: '一个人，只有永远拥有充满梦想和激-情的心灵，才能真正懂得生活的意义，也才能从真正的意义上享受生活！',
+    imgSrc: nat9
   }
 ];
 
@@ -24,6 +33,7 @@ export default function TourStoryList() {
           <div className="story">
             <figure className="story__shape">
               <img src={story.imgSrc} alt="旅游的人" className="story__img" />
+              <figcaption className="story__caption">{story.person}</figcaption>
             </figure>
 
             <div className="story__text">
@@ -35,6 +45,16 @@ export default function TourStoryList() {
           </div>
         </div>
       ))}
+
+      <div className="u-center-text u-margin-top-big">
+        <a
+          href="#"
+          onClick={e => e.preventDefault()}
+          className="btn-text"
+        >
+          发现所有故事 &rarr;
+        </a>
+      </div>
     </section>
   );
 };
