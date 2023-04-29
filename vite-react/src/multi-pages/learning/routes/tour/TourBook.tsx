2e7d3cc3fcd370484c8209769b29fbe1dfbca5c9
吴仙杰 <wuxianjiezh@gmail.com>
2023-04-29 11:49:21 +0800

    表单单选框样式

--- vite-react/src/multi-pages/learning/routes/tour/TourBook.tsx
+++ vite-react/src/multi-pages/learning/routes/tour/TourBook.tsx
@@ -6,7 +6,12 @@ export default function TourBook() {
       <div className="row">
         <div className="tour-book">
           <div className="tour-book__form">
-            <form action="#" className="form" autoComplete="off">
+            <form
+              action="#"
+              className="form"
+              autoComplete="off"
+              onSubmit={e => e.preventDefault()}
+            >
               <div className="u-margin-bottom-medium">
                 <h2 className="tour-heading-secondary">
                   立即预订
@@ -21,6 +26,28 @@ export default function TourBook() {
               <div className="form__group">
                 <input type="email" className="form__input" placeholder="邮箱" id="email" required />
                 <label htmlFor="email" className="form__label">邮箱</label>
+              </div>
+
+              <div className="form__group u-margin-bottom-medium">
+                <div className="form__radio-group">
+                  <input type="radio" className="form__radio-input" id="small" name="size" />
+                  <label htmlFor="small" className="form__radio-label">
+                    <span className="form__radio-button"></span>
+                    小型旅行团
+                  </label>
+                </div>
+
+                <div className="form__radio-group">
+                  <input type="radio" className="form__radio-input" id="large" name="size" />
+                  <label htmlFor="large" className="form__radio-label">
+                    <span className="form__radio-button"></span>
+                    大型旅行团
+                  </label>
+                </div>
+              </div>
+
+              <div className="form__group">
+                <button className="btn btn--green">下一步 &rarr;</button>
               </div>
             </form>
           </div>
