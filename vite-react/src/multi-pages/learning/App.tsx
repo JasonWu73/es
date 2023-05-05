37981d2179d32470764a86b4e229611e36e34973
吴仙杰 <wuxianjiezh@gmail.com>
2023-05-05 20:00:35 +0800

    Learning page

--- vite-react/src/multi-pages/learning/App.tsx
+++ vite-react/src/multi-pages/learning/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
+import {createHashRouter, RouterProvider} from 'react-router-dom';
 import Home from '@/multi-pages/learning/routes/home/Home';
 import SuspenseLoading from '@/components/loading/SuspenseLoading';
 import Root from '@/multi-pages/learning/Root';
@@ -7,20 +7,22 @@ import Root from '@/multi-pages/learning/Root';
 const RouteError = React.lazy(() => import('@/components/error/RouteError'));
 const Tour = React.lazy(() => import('@/multi-pages/learning/routes/tour/Tour'));
 const Grid = React.lazy(() => import('@/multi-pages/learning/routes/grid/Grid'));
+const Learning = React.lazy(() => import('@/multi-pages/learning/routes/learning/Learning'));
 
 const router = createHashRouter([
   {
     path: '/',
-    element: <Root />,
-    errorElement: <SuspenseLoading><RouteError /></SuspenseLoading>,
+    element: <Root/>,
+    errorElement: <SuspenseLoading><RouteError/></SuspenseLoading>,
     children: [
-      { index: true, element: <Home /> },
-      { path: 'tour', element: <SuspenseLoading><Tour /></SuspenseLoading> },
-      { path: 'grid', element: <SuspenseLoading><Grid /></SuspenseLoading> }
+      {index: true, element: <Home/>},
+      {path: 'tour', element: <SuspenseLoading><Tour/></SuspenseLoading>},
+      {path: 'grid', element: <SuspenseLoading><Grid/></SuspenseLoading>},
+      {path: 'learning', element: <SuspenseLoading><Learning/></SuspenseLoading>}
     ]
   }
 ]);
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router}/>;
 };
--- vite-react/src/multi-pages/learning/routes/home/Home.tsx
+++ vite-react/src/multi-pages/learning/routes/home/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useTitle } from '@/hooks/use-page';
-import { Link } from 'react-router-dom';
+import {useTitle} from '@/hooks/use-page';
+import {Link} from 'react-router-dom';
 
 export default function Home() {
   useTitle('Learning');
@@ -9,6 +9,7 @@ export default function Home() {
     <>
       <nav>
         <ul>
+          <li><Link to="/learning">Learning</Link></li>
           <li><Link to="/tour">去旅行吧 🏝️</Link></li>
           <li><Link to="/grid">栅格 Grid - 浮动 Float</Link></li>
         </ul>
